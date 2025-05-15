08005c5b31a501bc1730f705fe6d6081d00b6c5a
Cyrine Mejri <119369583+cyrinemejri@users.noreply.github.com>
2025-05-15 18:41:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/deployer-key.pub")
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-084568db4383264d4" 
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "DevOps-Instance"
