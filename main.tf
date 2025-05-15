97fb0ebb86b7ef11bbc1a28d64ef2ed41f9f5b90
Ubuntu <ubuntu@ip-172-31-33-35.ec2.internal>
2025-05-15 15:50:40 +0000

    Ajout Jnekins et configuration Terraform

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # À adapter à ta région
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DevOps-Instance"
+  }
+}
+
